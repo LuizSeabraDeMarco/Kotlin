df3a6eaeebfbe035f14e0f7e156455091e1e0fbd
LuizSeabraDeMarco <139667471+LuizSeabraDeMarco@users.noreply.github.com>
2023-08-04 19:30:23 -0300

    Initial commit

---
+++ ex01/ex01.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val teste = 1
+    var teste2 = 2
+    teste2 = 3
+    println("Ola Mundo!")
+    println(teste)
+    println(teste2)
+}
---
+++ ex02/ex02.kt
@@ -0,0 +1,28 @@
+import java.math.BigDecimal
+
+fun main(){
+    // Byte, Short, Int e Long
+    val byte: Byte= 127
+    val short: Short = 32767
+    val int: Int = 2147483647 
+    var long: Long = 9223372036854775807 
+
+    //Float e Double
+
+    var float: Float = 1.38423f //Tenque colocar "f" no final
+    var double: Double = 1.38423
+    var bigDecimal: BigDecimal = BigDecimal("1.38423")
+
+    //Boolean
+    val boolean: Boolean = false
+    val boolean2: Boolean = true
+
+    //Char
+    val char: Char = 'E'
+
+    //Strings
+    val string: String = "Luiz Filipe"
+
+    //Aray
+    val array: Array<Int> = arrayOf(0, 2, 4, 6, 8, 10)
+}
